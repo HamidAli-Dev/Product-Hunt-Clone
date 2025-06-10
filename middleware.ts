16f2920ac7620480edcdf6d8478b53aa10f6ccc7
Hamid Ali <hamidali.webdev@gmail.com>
2025-06-10 09:11:12 +0500

    Refactor middleware to remove session check for /new-product route and update matcher configuration. This simplifies access control by focusing on admin authentication only.

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,8 @@
-import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await auth();
 
-  // Protect routes under /new-product
-  if (request.nextUrl.pathname.startsWith("/new-product")) {
-    if (!session) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
 
   if ((await isAdminAuthenticated(request)) === false) {
     return new NextResponse("Unauthorized", {
@@ -45,5 +37,5 @@ async function isAdminAuthenticated(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*", "/new-product/:path*"]
+  matcher: ["/admin/:path*"]
 };
